1b5416acb4e7a67da2a7a003680f9dae139b61b0
uniyr08 <rahul@clickthrivemarketing.com>
2026-06-01 11:04:08 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    unoptimized: true,
+  },
+  compress: true,
+  poweredByHeader: false,
+  generateEtags: true,
+};
 
+module.exports = nextConfig;
